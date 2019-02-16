63642cd400dfc1af04fa9a4f9b9f84d6108a1f94
camjw <camjw119@gmail.com>
2019-02-16 21:01:45 +0000

    implemented instrumentation metrics

--- main.go
+++ main.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"github.com/camjw/stringsvc/stringsvc"
-	httptransport "github.com/go-kit/kit/transport/http"
-	"github.com/go-kit/kit/log"
 	"net/http"
 	"os"
+	"github.com/go-kit/kit/log"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	httptransport "github.com/go-kit/kit/transport/http"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
 )
 
 func main() {
 	logger := log.NewLogfmtLogger(os.Stderr)
 
+	fieldKeys := []string{"method", "error"}
+	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "request_count",
+		Help:      "Number of requests received.",
+	}, fieldKeys)
+	requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "request_latency_microseconds",
+		Help:      "Total duration of requests in microseconds.",
+	}, fieldKeys)
+	countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "count_result",
+		Help:      "The result of each count method.",
+	}, []string{})
+
 	var svc stringsvc.Service
 	svc = stringsvc.StringService{}
-  svc = stringsvc.LoggingMiddleware{logger, svc}
+	svc = stringsvc.LoggingMiddleware{logger, svc}
+  svc = stringsvc.InstrumentingMiddleware{requestCount, requestLatency, countResult, svc}
 
 	port := ":" + os.Getenv("TARGET_PORT")
 	if port == ":" {
@@ -34,6 +58,7 @@ func main() {
 
 	http.Handle("/uppercase", uppercaseHandler)
 	http.Handle("/count", countHandler)
+	http.Handle("/metrics", promhttp.Handler())
 
 	logger.Log("msg", "HTTP", "addr", port)
 	logger.Log("err", http.ListenAndServe(port, nil))
